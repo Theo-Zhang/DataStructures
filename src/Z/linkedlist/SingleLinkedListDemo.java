dd505a64a99a8329e0be4ad0bc598451e91bd13b
Theo-Zhang <zhangzhixun2016@foxmail.com>
2021-11-27 11:30:30 +0800

    单链表按顺序插入节点、修改更新

--- src/Z/linkedlist/SingleLinkedListDemo.java
+++ src/Z/linkedlist/SingleLinkedListDemo.java
@@ -1,21 +1,29 @@
 package Z.linkedlist;
 
+import java.lang.management.BufferPoolMXBean;
+
 public class SingleLinkedListDemo {
     public static void main(String[] args) {
         // 测试
         // 先创建节点
         HeroNode hero1 = new HeroNode(1,"宋江","及时雨");
-        HeroNode hero2 = new HeroNode(1,"卢俊义","玉麒麟");
-        HeroNode hero3 = new HeroNode(1,"吴用","智多星");
-        HeroNode hero4 = new HeroNode(1,"林冲","豹子头");
+        HeroNode hero2 = new HeroNode(2,"卢俊义","玉麒麟");
+        HeroNode hero3 = new HeroNode(3,"吴用","智多星");
+        HeroNode hero4 = new HeroNode(4,"林冲","豹子头");
 
         //创建一个链表
         SingleLinkedList singleLinkedList = new SingleLinkedList();
         //加入
-        singleLinkedList.add(hero1);
-        singleLinkedList.add(hero2);
-        singleLinkedList.add(hero3);
-        singleLinkedList.add(hero4);
+//        singleLinkedList.add(hero1);
+//        singleLinkedList.add(hero2);
+//        singleLinkedList.add(hero3);
+//        singleLinkedList.add(hero4);
+
+        singleLinkedList.addByOrder(hero1);
+        singleLinkedList.addByOrder(hero2);
+        singleLinkedList.addByOrder(hero3);
+        singleLinkedList.addByOrder(hero4);
+        singleLinkedList.addByOrder(hero4);
         //显示
         singleLinkedList.list();
     }
@@ -45,6 +53,33 @@ class SingleLinkedList{
         // 当退出while循环时，temp就指向链表的最后
         // 将最后这个节点的next 指向 新的节点
         temp.next = heroNode;
+    }
+
+    public void addByOrder(HeroNode heroNode){
+       // 因为头节点不能动，因此我们仍然通过一个辅助指针（变量）来帮助找到添加的位置
+        // 因为单链表中，我们找的temp是位于添加位置的前一个节点，否则插入不了
+        HeroNode temp = head;
+        boolean flag = false; // flag表征添加的编号是否存在，默认为false
+        while (true) {
+            if (temp.next == null) {// 说明temp已经在链表的最后
+                break;
+            }
+            if (temp.next.no > heroNode.no) {
+                break;
+            } else if (temp.next.no == heroNode.no) {//说明希望添加的heroNode的编号已经存在
+                flag = true; //说明编号存在
+                break;
+            }
+            temp = temp.next; // 后移遍历当前链表
+        }
+        // 判断flag的值
+        if(flag){ //
+            System.out.printf("准备插入的英雄编号已经%d存在，不能再加入\n",heroNode.no);
+        }else{
+            //插入到链表中，temp的后面
+            heroNode.next = temp.next;
+            temp.next = heroNode;
+        }
     }
 
     // 显示链表[遍历]
